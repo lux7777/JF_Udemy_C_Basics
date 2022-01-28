bae59b6f3247dc7144c55ade7c8e283644f1ffc2
Luka Obelic <luka.obelic@atinel.hr>
2022-01-28 12:55:55 +0100

    E2_Enum.c

--- E2_Enum.c
+++ E2_Enum.c
@@ -7,18 +7,18 @@
 // the program should display as output, the value of the three variables with each variable separated by a new line
 
 int main()
-{
-    enum Company {GOOGLE, FACEBOOK, XEROX, YAHOO, EBAY, MICROSOFT};
+{    
+    typedef enum Company {GOOGLE, FACEBOOK, XEROX, YAHOO, EBAY, MICROSOFT} Company;
 
-    enum Company xeCompany, goCompany, ebCompany;
+    Company xerox, google, ebay;
 
-    xeCompany = XEROX;
-    goCompany = GOOGLE;
-    ebCompany = EBAY;
+    xerox = XEROX;
+    google = GOOGLE;
+    ebay = EBAY;
 
-    printf("The value of XEROX = %d\n", xeCompany);
-    printf("The value of GOOGLE = %d\n", goCompany);
-    printf("The value of EBAY = %d\n", ebCompany);
+    printf("The value of XEROX = %d\n", xerox);
+    printf("The value of GOOGLE = %d\n", google);
+    printf("The value of EBAY = %d\n", ebay);
 
     return 0;
 }
